35a57b206dcccf75e8dc46ddd5e7b92cf4bc0edb
shishir <shishir.das@gmail.com>
2015-08-06 21:09:42 +0530

    add rebase commands

--- hellobot.js
+++ hellobot.js
@@ -2,16 +2,22 @@ module.exports = function(req, res, next) {
   var userName     = req.body.user_name;
   var incomingText = req.body.text;
   commands = incomingText.split(" ");
+  commands = "chuck foo bar baz".split(" ");
   releaseBranchName = commands[1];
 
-  // for (var i = 2; i < commands.lenght; i++) {
-
-  // }
   var megaText = "";
   megaText = "git checkout master\n";
   megaText += "git fetch origin\n";
   megaText += "git checkout -t -b "+ releaseBranchName + " origin/master\n";
+  megaText += "\n\n\n";
 
+  for (var i = 2; i < commands.length; i++) {
+    var branchCommand = "git checkout -t -b " + commands[i] + " origin/" + commands[i] + "\n";
+    branchCommand += "git rebase "+ releaseBranchName + " " + commands[i] + "\n";
+    branchCommand += "git rebase origin/"+ commands[i]; + " " + releaseBranchName + "\n";
+    branchCommand += "\n\n\n";
+    megaText += branchCommand;
+  }
 
   var botPayload = {
     text: megaText
